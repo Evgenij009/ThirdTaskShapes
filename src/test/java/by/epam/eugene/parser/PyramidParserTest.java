87f14df2941af8d9a7102dda0520d39380275d12
Eugene Shadura <evgeshashadura@gmail.com>
2021-07-25 08:59:08 +0300

    fix invalid data

--- src/test/java/by/epam/eugene/parser/PyramidParserTest.java
+++ src/test/java/by/epam/eugene/parser/PyramidParserTest.java
@@ -1,15 +1,15 @@
 package by.epam.eugene.parser;
 
 import by.epam.eugene.exception.PyramidException;
+import by.epam.eugene.parser.PyramidParser;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.Assert.assertEquals;
 
 public class PyramidParserTest {
     private PyramidParser parser;
@@ -20,15 +20,15 @@ public class PyramidParserTest {
     }
 
     @DataProvider(name = "valid_data")
-    public Object[][] pyramidValidDataCreate() {
-        return new Object[][] {
-                {Arrays.asList("1.0 2.0   3.0 0.5    5.0"), new Double[] {1.0, 2.0, 3.0, 0.5, 5.0}},
-                {Arrays.asList("1.0 2.0 3.0 2.32    23.0"), new Double[] {1.0, 2.0, 3.0, 2.32, 23.0}}
+    public Object[][] coneValidDataCreate() throws PyramidException {
+        return new Object[][]{
+                {Arrays.asList("1.0 1.0 0.0 6.0 4.0"), new double[]{1.0, 1.0, 0.0, 6.0, 4.0}},
+                {Arrays.asList("2.0 -1.0 2.0 9.0 5.0"), new double[]{2.0, -1.0, 2.0, 9.0, 5.0}}
         };
     }
 
-    @DataProvider(name = "invalid_data") //fixme
-    public Object[] pyramidInvalidDataCreate() {
+    @DataProvider(name = "invalid_data")
+    public Object[] coneInvalidDataCreate() throws PyramidException {
         return new Object[]{
                 Arrays.asList("1.0 1.0 0.0x 6.0 4.0"),
                 Arrays.asList("1.0c 1.0 0.0 6.0 4.0")
@@ -36,14 +36,13 @@ public class PyramidParserTest {
     }
 
     @Test(dataProvider = "invalid_data", expectedExceptions = PyramidException.class)
-    public void parsePyramidTest(List<String> lines) throws PyramidException {
-        parser.parseLineToDouble(lines);
+    public void parseConeTest(List<String> line) throws PyramidException {
+        parser.parseLineToDouble(line);
     }
 
     @Test(dataProvider = "valid_data")
-    public void parsePyramidTest(List<String> lines, double[] expected) throws PyramidException {
-        List<double[]> parsesList = parser.parseLineToDouble(lines);
-        assertEquals(parsesList.get(0), expected);
+    public void parseConeTest(List<String> line, double[] expected) throws PyramidException {
+        List<double[]> parsedList = parser.parseLineToDouble(line);
+        assertEquals(parsedList.get(0), expected);
     }
-
 }
